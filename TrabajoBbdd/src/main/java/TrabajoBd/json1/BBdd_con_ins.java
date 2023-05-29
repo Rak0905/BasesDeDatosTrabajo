d7354de17b40d3fee5f2c98bb7a915c7c414fab7
rak93 <raqueloles@gmail.com>
2023-05-29 13:05:46 +0200

    añadir el metodo de cerrar

--- TrabajoBbdd/src/main/java/TrabajoBd/json1/BBdd_con_ins.java
+++ TrabajoBbdd/src/main/java/TrabajoBd/json1/BBdd_con_ins.java
@@ -1,5 +1,59 @@
 package TrabajoBd.json1;
 
-public class BBdd_con_ins {
+/*
+ * Primero creamos el metodo para crear la conexion
+ */
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
+import javax.annotation.processing.Generated;
+
+public class BBdd_con_ins {
+	public Connection createConection() throws ClassNotFoundException, SQLException, IOException {
+		Connection connection = null;
+
+		try {
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://192.168.56.103:3306/socios?serverTimezone=UTC",
+					"furiosa", "Imperat0r!");
+
+			connection.setAutoCommit(false);
+
+			System.out.println("Se ha establecido conexion");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			throw e;
+
+		}
+
+		return connection;
+
+	}
+	/*
+	 * cerramos la conexion 
+	 */
+	public void diconnect(Connection connection) throws SQLException {
+		try {
+			if (null != connection) {
+				connection.close();
+				connection = null;
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+
+		}
+	}
 }
