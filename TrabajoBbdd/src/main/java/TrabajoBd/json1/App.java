5fa3612a712ad17fa4739a03f36e71920da99aab
autor <autor@gmail.com>
2023-05-29 12:57:30 +0200

    añadir al sorteo

--- TrabajoBbdd/src/main/java/TrabajoBd/json1/App.java
+++ TrabajoBbdd/src/main/java/TrabajoBd/json1/App.java
@@ -1,6 +1,8 @@
 package TrabajoBd.json1;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
@@ -19,6 +21,9 @@ public class App {
 		Jugador j = new Jugador(1, "trabajo@gmail.com","009997d", "1234A", 10);
 		Apuestas a = new Apuestas(1, "2080-05-09","2080-05-16:10:14", null, j, s);
 		File f = new File("Apuestas.json");
+		  String filePath = "loteria.json";
+	        String jsonString = readFileAsString(filePath);
+	        s.setCombinacionGanadora(jsonString);
 		try {
 			// creación del flujo de salida
 			PrintWriter printWriter = new PrintWriter(new FileWriter(f));
@@ -30,5 +35,20 @@ public class App {
 			System.out.println("Error: " + ex.getLocalizedMessage());
 		}
 	}
+	//para leer json y pasr a json
+	 public static String readFileAsString(String filePath) {
+	        StringBuilder stringBuilder = new StringBuilder();
+
+	        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+	            String line;
+	            while ((line = reader.readLine()) != null) {
+	                stringBuilder.append(line);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	        return stringBuilder.toString();
+	    }
 }
 
